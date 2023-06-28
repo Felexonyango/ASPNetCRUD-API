d0caf8e2aac9a01af57270103fe4fe14d4b053fd
Felexonyango <felexonyango19@gmail.com>
2023-06-28 19:56:36 +0300

    chores:updated crud functionality all

--- BookApp/Controllers/ProductController.cs
+++ BookApp/Controllers/ProductController.cs
@@ -28,5 +28,35 @@ namespace BookApp.Controllers
             return Ok();
         }
 
+        [HttpGet("allproducts")]
+        public IActionResult GetAllProducts()
+        {
+            var allproducts = _productService.GetAllProducts();
+            return Ok(allproducts);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _productService.GetProductById(id);
+              return Ok(product);
+        }
+
+        [HttpDelete("delete/{id}")]
+      
+        public IActionResult DeletProductById(int id)
+        {
+            _productService.DeleteProduct(id);
+            return Ok();
+        }
+        [HttpPut("update-product/{id}")]
+        public IActionResult UpdateProductById([FromBody] Product product, int id)
+        {
+            _productService.Update(product, id);
+            return Ok();
+        }
+
+
+
+
     }
 }
